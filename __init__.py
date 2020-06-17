22edba33524dbaf94ca89e02b425ab073e2e48cc
SkyLined <github@skylined.nl>
2020-06-17 10:46:21 +0200

    Exports pass down from mHTTPProtocol

--- __init__.py
+++ __init__.py
@@ -1,13 +1,21 @@
 from .cHTTPConnection import cHTTPConnection;
 from .cHTTPConnectionsToServerPool import cHTTPConnectionsToServerPool;
 from .cHTTPConnectionAcceptor import cHTTPConnectionAcceptor;
-from mHTTPProtocol import cHTTPRequest, cHTTPResponse, cURL;
+# Pass down
+from mHTTPProtocol import cHTTPHeader, cHTTPHeaders, cHTTPProtocolException, \
+    cHTTPRequest, cHTTPResponse, cInvalidMessageException, iHTTPMessage, cURL;
 
 __all__ = [
   "cHTTPConnection",
   "cHTTPConnectionsToServerPool",
   "cHTTPConnectionAcceptor",
+  # Pass down from mHTTPProtocol
+  "cHTTPHeader", 
+  "cHTTPHeaders", 
+  "cHTTPProtocolException",
   "cHTTPRequest",
   "cHTTPResponse",
+  "cInvalidMessageException",
+  "iHTTPMessage",
   "cURL",
 ];
