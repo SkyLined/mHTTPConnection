dd39e6e9d5f4884c250b6e593bd0359bfba6dc67
SkyLined <github@skylined.nl>
2020-06-17 17:47:33 +0200

    mDebugOutput is optional

--- cHTTPConnection.py
+++ cHTTPConnection.py
@@ -1,6 +1,15 @@
 import re;
 
-from mDebugOutput import ShowDebugOutput, fShowDebugOutput;
+try: # mDebugOutput use is Optional
+  from mDebugOutput import *;
+except: # Do nothing if not available.
+  ShowDebugOutput = lambda fxFunction: fxFunction;
+  fShowDebugOutput = lambda sMessage: None;
+  fEnableDebugOutputForModule = lambda mModule: None;
+  fEnableDebugOutputForClass = lambda cClass: None;
+  fEnableAllDebugOutput = lambda: None;
+  cCallStack = fTerminateWithException = fTerminateWithConsoleOutput = None;
+
 from mTCPIPConnections import cTransactionalBufferedTCPIPConnection;
 from mHTTPProtocol import cHTTPRequest, cHTTPResponse, iHTTPMessage;
 
